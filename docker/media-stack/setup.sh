cbb37588c03d181e6f10383d4dbc9a70483dba93
jack <45038833+jackra1n@users.noreply.github.com>
2023-10-04 18:59:17 +0200

    Fix typo

--- docker/media-stack/setup.sh
+++ docker/media-stack/setup.sh
@@ -7,7 +7,7 @@ sudo useradd prowlarr -u 13003
 sudo useradd bazarr -u 13004
 sudo useradd qbittorrent -u 13005
 sudo useradd plex -u 13006
-sudo useradd overseer -u 13007
+sudo useradd overseerr -u 13007
 sudo groupadd mediacenter -g 13000
 
 # Add users to group
@@ -17,10 +17,10 @@ sudo usermod -a -G mediacenter prowlarr
 sudo usermod -a -G mediacenter bazarr
 sudo usermod -a -G mediacenter qbittorrent
 sudo usermod -a -G mediacenter plex
-sudo usermod -a -G mediacenter overseer
+sudo usermod -a -G mediacenter overseerr
 
 # Create directories
-sudo mkdir -pv config/{sonarr,radarr,prowlarr,bazarr,qbittorrent,plex,overseer}-config
+sudo mkdir -pv config/{sonarr,radarr,prowlarr,bazarr,qbittorrent,plex,overseerr}-config
 sudo mkdir -pv data/{sonarr,radarr,prowlarr,bazarr,qbittorrent,plex}-data
 
 # Set permissions
@@ -32,5 +32,4 @@ sudo chown -R prowlarr:mediacenter config/prowlarr-config
 sudo chown -R bazarr:mediacenter config/bazarr-config
 sudo chown -R qbittorrent:mediacenter config/qbittorrent-config
 sudo chown -R plex:mediacenter config/plex-config
-sudo chown -R overseer:mediacenter config/overseer-config
-
+sudo chown -R overseerr:mediacenter config/overseerr-config
