fbae7aa328b50f96e3e23cbde6badfdad09bbd10
jackra1n <45038833+jackra1n@users.noreply.github.com>
2025-08-01 21:21:51 +0200

    i probably also wont need this

--- scripts/checkOpenFiles.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for pid in /proc/[0-9]*; do
-    p=$(basename $pid)
-    printf "%4d FDs for PID %6d; command=%s\n" $(ls $pid/fd | wc -l) $p "$(ps -p $p -o comm=)"
-done | sort -nr | head -10
